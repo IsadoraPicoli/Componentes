e3483a070d457c7e38fc397aade19099821ee591
IsadoraPicoli <62438268+IsadoraPicoli@users.noreply.github.com>
2022-09-22 05:09:13 -0400

    Criando gráfico linear

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import './data/amzn.json';
+import './data/npkgz.json';
+import './data/openjdk.json';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const data = [{Date: 'Page A', uv: 400, pv: 2400, Stargazers: 2400}, {Date: 'Page B', uv: 800, pv: 5000, Stargazers: 5000}, {Date: 'Page C', uv: 100, pv: 700, Stargazers: 700}]
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <LineChart width={600} height={300} data={data}>
+    <Line type="monotone" dataKey="uv" stroke="#ef476f" />
+    <Line type="monotone" dataKey="pv" stroke="#06d6a0" />
+    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+    <XAxis dataKey="Date" />
+    <YAxis dataKey="Stargazers" />
+    <Tooltip />
+  </LineChart>
   )
 }
 
