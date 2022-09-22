cb5ed9e4ac6c71bad9113e12c9378e8807954873
IsadoraPicoli <62438268+IsadoraPicoli@users.noreply.github.com>
2022-09-22 05:37:09 -0400

    Adicionando modelo do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import './data/amzn.json';
 import './data/npkgz.json';
 import './data/openjdk.json';
 
 function App() {
-  const data = [{Date: 'Page A', uv: 400, pv: 2400, Stargazers: 2400}, {Date: 'Page B', uv: 800, pv: 5000, Stargazers: 5000}, {Date: 'Page C', uv: 100, pv: 700, Stargazers: 700}]
+  const data = [{Starred_at: 'Page A', uv: 400, pv: 2400, Stargazers: 2400}, {Starred_at: 'Page B', uv: 800, pv: 5000, Stargazers: 5000}, {Starred_at: 'Page C', uv: 100, pv: 700, Stargazers: 700}]
   
   return (
     <LineChart width={600} height={300} data={data}>
     <Line type="monotone" dataKey="uv" stroke="#ef476f" />
     <Line type="monotone" dataKey="pv" stroke="#06d6a0" />
     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-    <XAxis dataKey="Date" />
+    <XAxis dataKey="Starred_at" />
     <YAxis dataKey="Stargazers" />
     <Tooltip />
+    <Legend />
   </LineChart>
   )
 }
