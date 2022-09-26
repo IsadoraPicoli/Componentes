00b34a9a4934437519ae92830e80c39e7ac92178
IsadoraPicoli <62438268+IsadoraPicoli@users.noreply.github.com>
2022-09-26 19:45:20 -0400

    conversões de dados, arrumar linhas: 33-36

--- src/App.jsx
+++ src/App.jsx
@@ -20,54 +20,56 @@ function App() {
   const transformedDataset = 
     originalData.map((data) => {
      data.starred_at = new Date(data.starred_at);
-
      data.day = data.starred_at.getDate();
-     data.quantidadeDia = _.countBy(transformedDataset, (record) => `${record.year}-${record.month}-${record.day}`);
-
      data.month = data.starred_at.getMonth();
-     data.quantidadeMes = _.countBy(transformedDataset, (record) => `${record.year}-${record.month}`);
-
      data.year = data.starred_at.getFullYear();
-     data.quantidadeAno = _.countBy(transformedDataset, 'year');
-
      return data; 
     });
 
+  quantidadeDia = _.countBy(transformedDataset, (record) => `${record.year}-${record.month}-${record.day}`);
+  quantidadeMes = _.countBy(transformedDataset, (record) => `${record.year}-${record.month}`);
+  quantidadeAno = _.countBy(transformedDataset, 'year');
+
+  const final = Object.entries(quantidadeAno).map(dado => {
+    'year', dado[0]
+    'quantidadeAno', dado[1]
+  });
+
   return (
     <div>
-      <h1>Visualização de dados temporais de estrelas no GitHub</h1>
+    <h1>Visualização de dados temporais de estrelas no GitHub</h1>
 
-      <div className="line-chart">
-        <LineChart width={400} height={400} data={transformedDataset}>
-          <Line type="monotone" dataKey="quantidadeAno" stroke="#ef476f" />
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <XAxis dataKey="year" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-        </LineChart>
+    <div className="line-chart">
+      <LineChart width={400} height={400} data={final}>
+        <Line type="monotone" dataKey="quantidadeAno" stroke="#ef476f" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="year" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+    </div>
+
+    <div className="container">
+      <div className="selector">
+        <label for="series_select" className="label">
+          <strong>Series: </strong>
+        </label>
+        <select id="series_select" onChange={handleChangeSeries}>
+          {SERIES.map(s => <option key={s} value={s}>By {s}</option>)}
+        </select>
       </div>
-
-      <div className="container">
-        <div className="selector">
-          <label for="series_select" className="label">
-            <strong>Series: </strong>
-          </label>
-          <select id="series_select" onChange={handleChangeSeries}>
-            {SERIES.map(s => <option key={s} value={s}>By {s}</option>)}
-          </select>
-        </div>
-        
-        <div className="selector">
-          <label for="scale_select" className="label">
-            <strong>Scale: </strong>
-          </label>
-          <select onChange={handleChangeScale}>
-            {SCALE.map(s => <option key={s} value={s}>{s}</option>)}
-          </select>
-        </div>
+      
+      <div className="selector">
+        <label for="scale_select" className="label">
+          <strong>Scale: </strong>
+        </label>
+        <select onChange={handleChangeScale}>
+          {SCALE.map(s => <option key={s} value={s}>{s}</option>)}
+        </select>
       </div>
     </div>
+  </div>
   )
 }
 
